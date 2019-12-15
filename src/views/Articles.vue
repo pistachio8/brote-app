e64348c112419e182650b0aa658048fad6beb98c
pistachio8 <wjaden0107@gmail.com>
2019-12-15 16:19:14 +0900

    Implement articles component

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,14 +1,48 @@
 <template>
   <div class="home">
-    <h2>Articles</h2>
-    <p>id: {{ getArticleId }}</p>
+    <Banner>
+      <h3>{{ article.title }}</h3>
+
+      <div class="article-meta">
+        <router-link :to="profileLink" class="profile-image">
+          <img :src="article.author.image" alt="author image" />
+        </router-link>
+        <div class="info">
+          <router-link :to="profileLink" class="article__author">{{
+            article.author.username
+          }}</router-link>
+          <small>{{ dateFormat }}</small>
+        </div>
+      </div>
+    </Banner>
+    <div class="article-content">{{ article.body }}</div>
   </div>
 </template>
 <script>
+import Banner from "@/components/Banner";
+import store from "@/store";
+import { FETCH_ARTICLE } from "@/store/actions.type";
+import { mapGetters } from "vuex";
+
 export default {
+  components: { Banner },
+  beforeRouteEnter(to, from, next) {
+    Promise.all([store.dispatch(FETCH_ARTICLE, to.params.slug)]).then(() => {
+      next();
+    });
+  },
   computed: {
-    getArticleId() {
-      return this.$route.params.id;
+    ...mapGetters(["article"]),
+    profileLink() {
+      return {
+        name: "profile",
+        params: {
+          username: this.article.author.username
+        }
+      };
+    },
+    dateFormat() {
+      return this.article.updatedAt.substring(0, 10);
     }
   }
 };
